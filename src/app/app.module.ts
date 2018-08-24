aaa8b950f496e7b18654db25c2ba5422be515b99
Harshvardhan Joshi <hj2931996@gmail.com>
2018-08-24 07:24:32 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { CourseComponent } from './course.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, CourseComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
