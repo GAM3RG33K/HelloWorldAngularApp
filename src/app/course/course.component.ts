cf8191427edd8cfbb652e14c757025905f6ab2ad
Harshvardhan Joshi <hj2931996@gmail.com>
2018-08-25 21:13:28 +0530

    1. Add bootstrap CSS import, 2. Click Event firing 3. Handling event propagation to upper levels

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,23 +1,41 @@
 import { CourseService } from './course.service';
-import { Component, OnInit } from '@angular/core';
+// tslint:disable-next-line:quotemark
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-course',
-  templateUrl: './course.component.html',
+  // templateUrl: './course.component.html',
+  template: `
+    <div (click)="divClick($event)">
+      <button class="btn btn-primary" (click)="saveButtonClick($event)" >Save</button>
+    </div>
+`,
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-
-  title  = 'Learn From the Course';
-  imageURL  = 'https://i.pinimg.com/originals/91/8b/39/918b3914a245d5407d44693a1c1478e1.jpg';
+  title = 'Learn From the Course';
+  imageURL =
+    'https://i.pinimg.com/originals/91/8b/39/918b3914a245d5407d44693a1c1478e1.jpg';
   courseList;
 
+  isDisabled = true;
 
-  constructor(service: CourseService ) {
+  constructor(service: CourseService) {
     this.courseList = service.getCourses();
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  saveButtonClick($event) {
+
+    // remove below comment to apply the bubble up event
+    // event of button click will also cause an event of click on wrapper div of this button
+    $event.stopPropagation();
+
+    console.log('Save button clicked!!', $event);
   }
 
+  divClick($event) {
+    console.log('Div clicked!!', $event);
+  }
 }
